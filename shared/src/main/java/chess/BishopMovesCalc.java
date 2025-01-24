bc860fd6e381f201e72cb8b366a507aec8578d55
kevin-dixon <87494457+kevin-dixon@users.noreply.github.com>
2025-01-23 20:04:54 -0700

    move calculator class initial

---
+++ shared/src/main/java/chess/BishopMovesCalc.java
@@ -0,0 +1,11 @@
+package chess;
+
+import java.util.Collection;
+
+public class BishopMovesCalc implements PieceMoveCalculator{
+
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        return null;
+    }
+}
---
+++ shared/src/main/java/chess/PieceMoveCalculator.java
@@ -0,0 +1,10 @@
+package chess;
+
+import java.util.Collection;
+
+public interface PieceMoveCalculator {
+
+    //methods
+    Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position);
+
+}
