ca71afcbc7fc7c472a4dd4ab2a9dbb14f3a2183b
kevin-dixon <87494457+kevin-dixon@users.noreply.github.com>
2025-03-03 19:19:22 -0700

    server update

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,9 +1,23 @@
 package server;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
 import server.handlers.*;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
 import spark.*;
 
 public class Server {
+
+    AuthDAO authDAO = new AuthDAO();
+    GameDAO gameDAO = new GameDAO();
+    UserDAO userDAO = new UserDAO();
+
+    ClearService clearService = new ClearService(authDAO, gameDAO, userDAO);
+    GameService gameService = new GameService(gameDAO);
+    UserService userService = new UserService(authDAO, userDAO);
 
     public Server() {
     }
@@ -15,9 +29,15 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db/:clear", new ClearHandler() {
 
-        Spark.delete("/clear", new ClearHandler());
-
+        });
+        Spark.post("/user/:register", new RegistrationHandler());
+        Spark.post("/session/:login", new LoginHandler());
+        Spark.delete("/session/:logout", new LogoutHandler());
+        Spark.get("/game/:list", new ListGamesHandler());
+        Spark.post("/game/:create", new CreateGameHandler());
+        Spark.put("/game/:join", new JoinGameHandler());
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
--- server/src/main/java/server/handlers/ClearHandler.java
+++ server/src/main/java/server/handlers/ClearHandler.java
@@ -1,8 +1,6 @@
 package server.handlers;
 
-import dataaccess.AuthDAO;
-import dataaccess.GameDAO;
-import dataaccess.UserDAO;
+import service.ClearService;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -11,6 +9,8 @@ public class ClearHandler implements Route {
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        return "\nImplement Clear Handling";
+
+
+        return response;
     }
 }
